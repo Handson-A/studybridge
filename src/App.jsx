60af740daac5caafd0b6ef4d4ab196a421ee1480
Handson-A <hayelgum@st.ug.edu.gh>
2026-01-24 18:05:30 +0000

    chore: initial Vite + React scaffold for StudyBridge

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="app">
+      <header className="appHeader">
+        <h1>StudyBridge</h1>
+      </header>
+
+      <main className="appMain">
+        <p>Your study companion.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
